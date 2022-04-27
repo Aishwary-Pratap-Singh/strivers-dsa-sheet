668a30b24217343c6d83215aba91f641dba219aa
Aishwary-Pratap-Singh <66994420+Aishwary-Pratap-Singh@users.noreply.github.com>
2022-04-27 12:57:39 +0530

    Create dfs.py

---
+++ graph/dfs.py
@@ -0,0 +1,18 @@
+    def f(self,node,vis,adj,dfs):
+        dfs.append(node)
+        vis[node] = True
+        for i in adj[node]:
+            if vis[i] == False:
+                self.f(i,vis,adj,dfs)
+            
+        
+        
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfsOfGraph(self, V, adj):
+        # code here
+        dfs = []
+        vis = [False for i in range(V)]
+        for i in range(V):
+            if vis[i] == False:
+                self.f(i,vis,adj,dfs)
+        return dfs
